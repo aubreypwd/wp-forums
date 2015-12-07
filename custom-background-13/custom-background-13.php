7149495e268219bc44a721128dc8944a9328a8cd
Aubrey Portwood <aubreypwd@gmail.com>
2015-12-07 08:44:07 -0700

    My solution.

--- custom-background-13/custom-background-13.php
+++ custom-background-13/custom-background-13.php
@@ -2,34 +2,44 @@
 
 add_theme_support( 'custom-background' );
 
-add_action( 'customize_register', 'custom_background_size' );
-
 function custom_background_size( $wp_customize ) {
 
-	$wp_customize->add_setting( 'default-size', array(
-			'default' => 'inherit',
-		) );
+	// Add the "panel" (Section).
+	// If this section already exists, comment the next 3 lines out.
+	$wp_customize->add_section( 'theme_settings', array(
+		'title' => __( 'Theme Settings' ),
+	) );
 
+	// If they haven't set the background image, don't show these controls.
+	if ( ! get_theme_mod( 'background_image' ) ) {
+		return;
+	}
+
+	// Add your setting.
+	$wp_customize->add_setting( 'default-size', array(
+		'default' => 'inherit',
+	) );
+
+	// Add your control box.
 	$wp_customize->add_control( 'default-size', array(
-			'label'      => 'Background Image Size',
-			'section'    => 'background_image',
-			'settings'   => 'default-size',
-			'priority'   => 200,
-			'type' => 'radio',
-			'choices' => array(
-				'cover' => 'Cover',
-				'contain' => 'Contain',
-				'inherit' => 'Inherit',
-			)
-		));
+		'label'      => __( 'Background Image Size' ),
+		'section'    => 'theme_settings',
+		'settings'   => 'default-size',
+		'priority'   => 200,
+		'type' => 'radio',
+		'choices' => array(
+			'cover' => __( 'Cover' ),
+			'contain' => __( 'Contain' ),
+			'inherit' => __( 'Inherit' ),
+		)
+	) );
+}
+
+add_action( 'customize_register', 'custom_background_size' );
+
+function custom_background_size_css() {
+	$background_size = get_theme_mod( 'default-size', 'inherit' );
+	echo '<style> body.custom-background { background-size: '.$background_size.'; } </style>';
 }
 
 add_action( 'wp_head', 'custom_background_size_css', 999 );
-
-function custom_background_size_css() {
-
-	$background_size = get_theme_mod( 'default-size', 'inherit' );
-
-	echo '<style type="text/css"> body.custom-background { background-size:'.$background_size.'; } </style>';
-
-}
--- wp-forums.php
+++ wp-forums.php
@@ -25,4 +25,4 @@ License: GPL2
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-include "custom-background-13/custom-background-13/php";
+include "custom-background-13/custom-background-13.php";
