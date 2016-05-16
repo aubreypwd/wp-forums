990e6fd0a9ead943e9af75ba3c65783b6b063a70
Aubrey Portwood <aubreypwd@gmail.com>
2016-05-16 08:44:11 -0700

    Show terms from 80 days
    What this does is gets the most recent posts in the last 90 days, then
    from those posts, gets their terms for the taxonomies we specify, then
    it outputs those terms in a list.

--- wp-forums.php
+++ wp-forums.php
@@ -25,3 +25,129 @@ License: GPL2
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+class WP_Forums_aubreypwd_Ninety_Terms extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc.
+	 *
+	 * @author Aubrey Portwood
+	 */
+	public function __construct() {
+		$widget_ops = array(
+			'classname' => 'wp-forums-aubreypwd-ninety-terms',
+			'description' => __( 'Shows terms used in last 90 days' ),
+		);
+
+		parent::__construct( 'wp-forums-aubreypwd-ninety-terms', __( '90 Days Terms' ), $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget.
+	 *
+	 * @author Aubrey Portwood
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+
+		// Before
+		echo $args['before_widget'];
+
+		// The posts we're going to get.
+		$query_args = array(
+			'post_type'    => apply_filters( 'wp_forums_aubreypwd_ninety_terms/post_type', array( 'post' ) ),
+			'post_status'  => apply_filters( 'wp_forums_aubreypwd_ninety_terms/post_status', array( 'publish' ) ),
+
+			// Just the id's.
+			'fields'       => 'ids',
+
+			// Only posts within last 90 days.
+			'date_query' => apply_filters( 'wp_forums_aubreypwd_ninety_terms/date_query', array(
+				array(
+					'column' => 'post_date_gmt',
+					'after'  => '90 days ago',
+				),
+			) ),
+		);
+
+		// Get those posts.
+		$posts = get_posts( $query_args );
+
+		// The taxonomies to use, add your custom taxonomy here or use filter.
+		$taxonomies = apply_filters( 'wp_forums_aubreypwd_ninety_terms/taxonomies', array( 'category' ) );
+
+		// For each of these taxonomies.
+		foreach ( $taxonomies as $taxonomy ) {
+
+			// Collect the terms from all the posts.
+			foreach ( $posts as $post_id ) {
+				$post_terms[ $taxonomy ][ $post_id ] = wp_get_post_terms( $post_id, $taxonomy, array(
+					'fields' => 'ids',
+				) );
+			}
+		}
+
+		if ( ! isset( $post_terms ) ) {
+			return; // Nothing to show.
+		}
+
+		// Output the list.
+		?><ul class="terms"><?php
+
+			// Go thorough the terms.
+			foreach ( $post_terms as $taxonomy => $terms ) {
+				foreach ( $terms as $term_id ) {
+
+					// Get the term info.
+					$term = get_term( $term_id, $taxonomy, OBJECT );
+
+					// Output this category.
+					?><li class="term-<?php echo absint( $term_id ); ?> term-<?php echo esc_attr( $term->slug ); ?>"><?php echo esc_html( $term->name ); ?></li><?php
+				}
+			}
+
+		?></ul><?php
+
+		// After
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Outputs the options form on admin.
+	 *
+	 * @author Aubrey Portwood
+	 *
+	 * @param array $instance The widget options.
+	 */
+	public function form( $instance ) {
+		echo sprintf( __( '%s Place this widget where you want terms to show. %s' ), '<p>', '</p>' );
+	}
+
+	/**
+	 * Processing widget options on save.
+	 *
+	 * @author Aubrey Portwood
+	 *
+	 * @param array $new_instance The new options.
+	 * @param array $old_instance The previous options.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// No widget options.
+	}
+}
+
+/**
+ * Register the Widget.
+ *
+ * @author  Aubrey Portwood
+ *
+ * @return boolean True if we register the widget, false if not.
+ */
+function wp_forums_aubreypwd_ninety_terms() {
+	return register_widget( 'WP_Forums_aubreypwd_Ninety_Terms' );
+}
+
+// Register the widget on this hook.
+add_action( 'widgets_init', 'wp_forums_aubreypwd_ninety_terms' );
