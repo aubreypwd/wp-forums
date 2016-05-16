aac2d384174d39b3ed5c9abbd2fcfd7ef1d56cf6
Aubrey Portwood <aubreypwd@gmail.com>
2016-05-16 07:46:49 -0700

    Clean up README
    The README needs more info

--- readme.md
+++ readme.md
@@ -1,8 +1,15 @@
 # wp-forums
 
+![](http://assets.fontsinuse.com/static/use-media-items/7/6289/full-602x104/56701d47/wp-header-logo-2x.png?resolution=0)
+
 This is where I test/write code solutions for people on the WordPress.org
 Support Forums.
 
-Each forum topic has it's own matching branch. E.g.
-`https://wordpress.org/support/topic/custom-background-13` has a solution on
-`custom-background-13` branch.
+You can checkout my profile at: [https://wordpress.org/support/profile/aubreypwd](https://wordpress.org/support/profile/aubreypwd)
+You can follow me on Twitter at: [http://twitter.com/aubreypwd](http://twitter.com/aubreypwd)
+
+# Braches equal Topics
+
+Each forum topic has it's own [matching branch](https://github.com/aubreypwd/wp-forums/branches). E.g.
+`https://wordpress.org/support/topic/help-me` has a solution on
+the `topic/help-me` branch.
--- wp-forums.php
+++ wp-forums.php
@@ -1,26 +1,27 @@
 <?php
 /*
 Plugin Name: wp-forums
-Plugin URI:
+Plugin URI: https://github.com/aubreypwd/wp-forums/
 Description:
 Version:
 Author: Aubrey Portwood
-Author URI: http://aubreypwd.com
+Author URI: http://twitter.com/aubreypwd
 License: GPL2
 */
 
-/*  Copyright 2015 Aubrey Portwood <aubreypwd@gmail.com>
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as
-	published by the Free Software Foundation.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/*
+ * Copyright 2015 Aubrey Portwood <aubreypwd@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
